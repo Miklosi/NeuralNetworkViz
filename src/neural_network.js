2577978d59686c44214043abd08e195214ed0fad
Nick Imrei <nick.imrei@kiandra.com.au>
2015-06-22 21:01:35 +1000

    -Fixed bug in back-propagation algorithm. -Fixed bug in weight/bias value initialization. -Added MathJS chaining for better readability.

--- src/neural_network.js
+++ src/neural_network.js
@@ -1,4 +1,3 @@
-//var Network 
 
 var NeuralNetwork = function(sizes) { 
 
@@ -19,20 +18,19 @@ var NeuralNetwork = function(sizes) {
 						});
 		};
 
-		/*
+
 		//sizes: [4,5,6,7] //no. neurons at each layer
-		self.biases = self.sizes.slice(1, self.sizes.length)
-		                   .map(function(x, i) { 
-		                   	  return matrixApply(math.ones([x, 1]), initialiseFn);
+		self.biases =   self.sizes.slice(1, self.sizes.length)
+		                    .map(function(x, i) { 
+		                   	  return math.matrix(matrixApply(math.ones([x, 1]), initialiseFn));
 		                    });
 		self.weights = self.sizes.slice(1, self.sizes.length)
 		                   .map(function(x, i) { 
-								return matrixApply(math.ones([x, sizes[i]]), initialiseFn)
+								return math.matrix(matrixApply(math.ones([x, self.sizes[i]]), initialiseFn));
 		                    });
-		*/
 
-		//TEST DATA - replace math.matrix with array keyword in IPython
-		//https://www.pythonanywhere.com/try-ipython/
+/*
+		//TEST DATA
 		self.biases = [
 		    math.matrix([[0.262628827476874], [0.0737376578617841]])
 		    , 
@@ -45,6 +43,8 @@ var NeuralNetwork = function(sizes) {
 		    math.matrix([[0.23425847105681896, 0.6160134135279804]])
 		];
 
+		var e = 1;*/
+
 	};
 
 	//https://github.com/mnielsen/neural-networks-and-deep-learning/blob/master/src/network.py
@@ -54,7 +54,7 @@ var NeuralNetwork = function(sizes) {
 		var n = training_data.length;
 
 		for (var j = 1; j <= epochs; j++) {
-			var epoch_training_data = d3.shuffle(training_data);
+			var epoch_training_data = training_data; //d3.shuffle(training_data);
 
 		  	var mini_batches = [];
 		  	for (var k=0; k < n; k += mini_batch_size) {
@@ -86,7 +86,12 @@ var NeuralNetwork = function(sizes) {
 //			return d.target[0][0] == argmax ? 1 : 0;
 
 			//evaluation for AND/OR/etc. logic gates:
-			return feedForward(d.value)._data[0][0] == d.target[0][0] ? 1 : 0;
+			var target = d.target[0][0],
+				actual = feedForward(d.value)._data[0][0];
+
+			console.log("input: %s, output: %f, target: %f", math.format(d.value), actual, target);
+
+			return actual.toFixed(3) == target.toFixed(3) ? 1 : 0;
  		});
 
 		return parseFloat(d3.sum(num_correct)) / test_data.length;
@@ -95,10 +100,9 @@ var NeuralNetwork = function(sizes) {
 	var feedForward = function(a) { //a: inputs, eg.: [[1],[1]]
 		for (var i = 0; i < self.weights.length; i++) {
 		  a = sigmoid_vec(
-		        math.add(
-		          math.multiply(self.weights[i], a), 
-		          self.biases[i]
-		        )
+				math.chain(self.weights[i])
+					.multiply(a)
+					.add(self.biases[i]).done()
 		      ); 
 		}
 		return a;
@@ -117,33 +121,43 @@ var NeuralNetwork = function(sizes) {
 		for (var k = 0; k < self.biases.length; k++) {
 		    var w = self.weights[k], 
 		        b = self.biases[k];
-		    var z = math.add(math.multiply(w, activation), b);
+		    var z = math.chain(w)
+		    			.multiply(activation)
+		    			.add(b).done();
 
 		    zs.push(z);
 		    activations.push(sigmoid_vec(z));
 		}
 
 		//backward-pass
-		    //delta = cost_derivative(activations[-1], y) * sigmoid_prime_vec(zs[-1])
-		var delta = math.dotMultiply(
-								  cost_derivative(activations[activations.length-1], y),
-		              			  sigmoid_prime_vec(zs[zs.length-1])
-		              			 );
+
+		//output error
+		var delta = math.chain(cost_derivative(activations[activations.length-1], y))
+						.dotMultiply(sigmoid_prime_vec(zs[zs.length-1])).done();
+
 		nabla_b[nabla_b.length-1] = delta;
-		nabla_w[nabla_w.length-1] = math.multiply(delta, math.transpose(activations[activations.length-2]));
+		nabla_w[nabla_w.length-1] = math.chain(delta)
+										.multiply(
+											math.chain(activations[activations.length-2])
+												.transpose().done()
+										).done();
 
 		for (var l = 2; l < self.num_layers; l++) {
 			var z = zs[zs.length-l];
 			var spv = sigmoid_prime_vec(z);
 
-			//delta = np.dot(self.weights[-l+1].transpose(), delta) * spv
-			delta = math.dotMultiply(math.multiply(math.transpose(self.weights[self.weights.length-(l-1)]), delta), spv);
+			//error at layer 'l'
+			delta = math.chain(self.weights[self.weights.length-(l-1)])
+						.transpose()
+						.multiply(delta)
+						.dotMultiply(spv).done();
 
-			//nabla_b[-l] = delta
 			nabla_b[nabla_b.length-l] = delta; 
-
-			////nabla_w[-l] = np.dot(delta, activations[-l-1].transpose())
-			nabla_w[nabla_w.length-l] = math.multiply(delta, math.transpose(activations[activations.length-l]));
+			nabla_w[nabla_w.length-l] = math.chain(delta)
+											.multiply(
+												math.chain(activations[activations.length-(l+1)])
+													.transpose().done()
+											).done();
 		  
 		}
 
@@ -151,7 +165,6 @@ var NeuralNetwork = function(sizes) {
 	};
 	
 	var update_mini_batch = function(mini_batch, eta) {
-		//console.log(math.format(mini_batch));
 
 		var nabla_b = zeroMatrixCopyOf(self.biases);
 		var nabla_w = zeroMatrixCopyOf(self.weights);
@@ -160,13 +173,9 @@ var NeuralNetwork = function(sizes) {
 		  var x = mini_batch[m].value, 
 		      y = mini_batch[m].target; 
 
-		  var backprop_result = self.backprop(x, y);
+		  var backprop_result = backprop(x, y);
 		  var delta_nabla_b = backprop_result[0],
 		      delta_nabla_w = backprop_result[1];
-
-		  //temp solution until self.backprop() is written!!
-		  //var delta_nabla_b = oneMatrixCopyOf(self.biases);
-		  //var delta_nabla_w = oneMatrixCopyOf(self.weights);
 
 		  nabla_b = nabla_b.map(function(x, i) { return math.add(nabla_b[i], delta_nabla_b[i]) });
 		  nabla_w = nabla_w.map(function(x, i) { return math.add(nabla_w[i], delta_nabla_w[i]) });
